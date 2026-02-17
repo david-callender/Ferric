a2922cb1458beff0af754f5d8fcbd80a40e62acf
Sameer Gupta <sameerfgupta@gmail.com>
2026-02-16 19:53:07 -0600

    Basic lexer with tokens

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,9 +1,39 @@
 use std::iter::Peekable;
 
-pub enum Token {}
+pub enum Token {
+    Let,          // let
+    If,           // if
+    Elseif,       // elseif
+    Otherwise,    // otherwise
+    While,        // while
+    Fn,           // fn
+    OpenParen,    // (
+    CloseParen,   // )
+    OpenBracket,  // {
+    CloseBracket, //, }
+    Sem,          // ;
+    Comma,        // ,
+    Eq,           // =
+    EqEq,         // =”
+    BangEq,       // !”
+    Plus,         // +
+    Minus,        // -
+    Star,         // *
+    Slash,        // /
+    Greater,      // >
+    GreaterEq,    // >”
+    Less,         // <
+    LessEq,       // <”
+    LAnd,         // and
+    LOr,          // or
+    Bang,         // !
+    StringLit(String),
+    NumLit(f64),
+    Ident(String),
+}
 
 pub struct Lexer<I: Iterator<Item = u8>> {
-    stream: Peekable<I>
+    stream: Peekable<I>,
 }
 
 impl<I: Iterator<Item = u8>> Iterator for Lexer<I> {
@@ -16,7 +46,7 @@ impl<I: Iterator<Item = u8>> Iterator for Lexer<I> {
 impl<I: Iterator<Item = u8>> Lexer<I> {
     pub fn new(stream: I) -> Self {
         Self {
-            stream: stream.peekable()
+            stream: stream.peekable(),
         }
     }
 }

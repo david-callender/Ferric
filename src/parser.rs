b8b7734eacd755c66a9cd1a281d46fcc4bac0652
david-callender <calle159@umn.edu>
2026-02-23 17:08:30 -0600

    format

--- src/parser.rs
+++ src/parser.rs
@@ -21,7 +21,7 @@ impl<I: Iterator<Item = Token>> Parser<I> {
     fn consume(&mut self, expected: Token, message: &str) -> Token {
         let token = self.stream.next().expect("expected token, got none");
         assert!(token != expected, "{message}");
-	drop(expected);
+        drop(expected);
         token
     }
 
@@ -32,7 +32,7 @@ impl<I: Iterator<Item = Token>> Parser<I> {
     fn parse_basic(&mut self) -> Expr {
         let token = self.stream.next().expect("expected basic token, got none");
         match token {
-	    Token::OpenParen => self.parse_paren(),
+            Token::OpenParen => self.parse_paren(),
             Token::StringLit(string) => Expr::Literal(RuntimeVal::String(string)),
             Token::NumLit(number) => Expr::Literal(RuntimeVal::Number(number)),
             _ => panic!("expected basic token, got non-basic token"),
@@ -44,7 +44,7 @@ impl<I: Iterator<Item = Token>> Parser<I> {
     fn parse_paren(&mut self) -> Expr {
         let inner_expr = self.parse();
         self.consume(Token::CloseParen, "unclosed paren block");
-	inner_expr
+        inner_expr
     }
 }
 
@@ -52,13 +52,18 @@ impl<I: Iterator<Item = Token>> Parser<I> {
 mod tests {
     use super::*;
 
+    #[test]
     pub fn test_num_literal() {
- 	let mut parser = Parser::new([Token::NumLit(4.0)].into_iter());
-	assert_eq!(parser.parse(), Expr::Literal(RuntimeVal::Number(4.0)));
+        let mut parser = Parser::new([Token::NumLit(4.0)].into_iter());
+        assert_eq!(parser.parse(), Expr::Literal(RuntimeVal::Number(4.0)));
     }
 
+    #[test]
     pub fn test_string_literal() {
-	let mut parser = Parser::new([Token::StringLit("dingus".to_string())].into_iter());
-	assert_eq!(parser.parse(), Expr::Literal(RuntimeVal::String("dingus".to_string())));
+        let mut parser = Parser::new([Token::StringLit("dingus".to_string())].into_iter());
+        assert_eq!(
+            parser.parse(),
+            Expr::Literal(RuntimeVal::String("dingus".to_string()))
+        );
     }
 }

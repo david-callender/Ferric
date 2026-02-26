52f0fa33325a53bef2e744913169e494e696837e
david-callender <calle159@umn.edu>
2026-02-26 15:55:07 -0600

    bad test

--- src/parser.rs
+++ src/parser.rs
@@ -114,6 +114,7 @@ mod tests {
 
     #[test]
     pub fn test_num_literal() {
+        panic!("testing");
         let mut parser = Parser::new([Token::NumLit(4.0)].into_iter());
         assert_eq!(parser.parse(), Expr::Literal(RuntimeVal::Number(4.0)));
     }

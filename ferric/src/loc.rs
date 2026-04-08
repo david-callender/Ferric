a701ec855088917385962f26d4e5109b7a739ee9
david-callender <calle159@umn.edu>
2026-04-07 19:49:59 -0500

    clean up multiline errors

--- ferric/src/loc.rs
+++ ferric/src/loc.rs
@@ -1,5 +1,3 @@
-use std::fmt::format;
-
 fn num_digits(n: usize) -> usize {
     n.checked_ilog10().unwrap_or(0) as usize + 1
 }
@@ -26,8 +24,34 @@ impl ProgramSrc {
         Self { src, line_nums }
     }
 
-    pub fn get_line_nums(&self) -> &Vec<usize> {
-        &self.line_nums
+    fn get_line(&self, line: usize) -> Option<ProgramLine<'_>> {
+        if line == 0 {
+            return None;
+        }
+        let first = *self.line_nums.get(line)?;
+        let last = *self.line_nums.get(line + 1)?;
+        let contents = &self.src[first..last - 1]; // remove final newline
+        Some(ProgramLine {
+            contents,
+            num: line,
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+struct ProgramLine<'a> {
+    contents: &'a str,
+    num: usize,
+}
+
+impl ProgramLine<'_> {
+    fn display(&self, gutter_size: usize, post_gutter: &str, pre_line: &str) -> String {
+        format!(
+            "{}{}{post_gutter} | {pre_line}{}",
+            " ".repeat(gutter_size - num_digits(self.num)),
+            self.num,
+            self.contents
+        )
     }
 }
 
@@ -43,46 +67,25 @@ impl Loc {
         Self { line, col }
     }
 
-    fn get_line(src: &ProgramSrc, line: usize) -> Option<&str> {
-        if line == 0 {
-            return None;
-        }
-        let line_nums = src.get_line_nums();
-        let first = *line_nums.get(line)?;
-        let last = *line_nums.get(line + 1)?;
-        Some(&src.src[first..last - 1]) // remove final newline
-    }
-
     pub fn format_with_len(&self, src: &ProgramSrc, message: &str, len: usize) -> String {
-        let prev = Self::get_line(src, self.line - 1);
-        let this = Self::get_line(src, self.line);
-        let next = Self::get_line(src, self.line + 1);
+        let prev = src.get_line(self.line - 1);
+        let this = src.get_line(self.line);
+        let next = src.get_line(self.line + 1);
 
-        let prev_num_len = num_digits(self.line - 1);
-        let this_num_len = num_digits(self.line);
-        let next_num_len = num_digits(self.line + 1);
+        let gutter_size = num_digits(self.line + 1);
 
-        let num_space = next_num_len;
-
-        let make_line = |line: Option<&str>, num_len| {
-            line.map(|prev| {
-                format!(
-                    "{}{} | {}",
-                    " ".repeat(num_space - num_len),
-                    self.line - 1,
-                    prev
-                )
-            })
-            .unwrap_or_default()
+        let make_line = |line: Option<ProgramLine<'_>>| {
+            line.map(|line| line.display(gutter_size, "", ""))
+                .unwrap_or_default()
         };
 
-        let prev_fmt = make_line(prev, prev_num_len);
-        let this_fmt = make_line(this, this_num_len);
-        let next_fmt = make_line(next, next_num_len);
+        let prev_fmt = make_line(prev);
+        let this_fmt = make_line(this);
+        let next_fmt = make_line(next);
 
         let underline = format!(
             "{}|{}{} {}",
-            " ".repeat(num_space + 1),
+            " ".repeat(gutter_size + 1),
             " ".repeat(self.col + 1),
             "^".repeat(len),
             message
@@ -90,7 +93,7 @@ impl Loc {
 
         format!("{prev_fmt}\n{this_fmt}\n{underline}\n{next_fmt}")
     }
-    
+
     pub fn format(&self, src: &ProgramSrc, message: &str) -> String {
         self.format_with_len(src, message, 1)
     }
@@ -107,30 +110,44 @@ impl Span {
         assert!(end >= start);
         Self { start, end }
     }
-    
+
     pub fn format(&self, src: &ProgramSrc, message: &str) -> String {
         if self.start.line == self.end.line {
-            return self.start.format_with_len(src, message, self.end.col - self.start.col);
+            return self
+                .start
+                .format_with_len(src, message, self.end.col - self.start.col);
         }
-        
-        let prev = Loc::get_line(src, self.start.line - 1);
-        let start = Loc::get_line(src, self.start.line);
-        
-        let middle = (self.start.line+1..=self.end.line).map(|i| (i, Loc::get_line(src, i)));
-        
-        let next = Loc::get_line(src, self.end.line + 1);
-        
+
+        let prev = src.get_line(self.start.line - 1);
+        let start = src.get_line(self.start.line);
+        let middle = (self.start.line + 1..=self.end.line).map(|i| src.get_line(i));
+        let next = src.get_line(self.end.line + 1);
+
         let num_space = num_digits(self.end.line + 1);
-        
-        let prev_fmt = prev.map(|prev| format!("{}{} |   {}", " ".repeat(num_space - num_digits(self.start.line - 1)), self.start.line - 1, prev)).unwrap_or_default();
-        let start_fmt = start.map(|start| format!("{}{} |   {}", " ".repeat(num_space - num_digits(self.start.line)), self.start.line, start)).unwrap_or_default();
-        let start_underline = format!("{}|  {}^", " ".repeat(num_space+1), "_".repeat(self.start.col + 1));
-        
-        let middle_fmt = middle.map(|(i, line)| line.map(|line| format!("{}{} | | {}", " ".repeat(num_space - num_digits(i)), i, line)).unwrap_or_default() ).collect::<Vec<_>>();
-        
-        let end_underline = format!("{}| |{}^", " ".repeat(num_space+1),"_".repeat(self.end.col + 1));
-        let next_fmt = next.map(|next| format!("{}{} |   {}", " ".repeat(num_space - num_digits(self.end.line + 1)), self.end.line + 1, next)).unwrap_or_default();
-        
-        format!("{prev_fmt}\n{start_fmt}\n{start_underline}\n{}\n{end_underline}\n{next_fmt}", middle_fmt.join("\n"))
+
+        let make_line = |line: Option<ProgramLine<'_>>, pre_line: &str| {
+            line.map(|line| line.display(num_space, "", pre_line))
+                .unwrap_or_default()
+        };
+
+        let prev_fmt = make_line(prev, "  ");
+        let start_fmt = make_line(start, "  ");
+        let start_underline = format!(
+            "{}|  {}^",
+            " ".repeat(num_space + 1),
+            "_".repeat(self.start.col + 1)
+        );
+        let middle_fmt = middle.map(|line| make_line(line, "| ")).collect::<Vec<_>>();
+        let end_underline = format!(
+            "{}| |{}^",
+            " ".repeat(num_space + 1),
+            "_".repeat(self.end.col + 1)
+        );
+        let next_fmt = make_line(next, "  ");
+
+        format!(
+            "{prev_fmt}\n{start_fmt}\n{start_underline}\n{}\n{end_underline}\n{next_fmt}",
+            middle_fmt.join("\n")
+        )
     }
 }
